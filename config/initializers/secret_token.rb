dd835d6ea7e98401d7ca05adbae89ac933bcd441
Russ Smith <russ@bashme.org>
2011-02-16 18:38:38 -0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrapQueue::Application.config.secret_token = 'bf18eedad9912f53634da7ced9f55d62b89574aebdb3851bf212345c9a0de377584b0f00f103a4f80d69d02e9e88df0cf36c72f7bca0c773f0e0c8406900243c'
