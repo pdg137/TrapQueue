176434246d32d05d1381b116dac9f4524f2caa4a
Ryan Mulligan <ryan@ryantm.com>
2011-04-27 20:01:23 -0700

    removed some exceptions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,11 +23,9 @@ class UsersController < ApplicationController
   
   def update
     @user = User.find(params[:id])
-    raise params[:checked].inspect
     if params[:checked] == "1"
       @user.has_role!(params[:role])
     else
-      raise params[:role].inspect
       @user.has_no_role!(params[:role])
     end
     render :nothing => true
