4259e2b7e45ef7360735f0e60220fa93f8e6f80b
Paul Grayson <pdg@alum.mit.edu>
2011-04-27 18:41:49 -0700

    added Role.all_role_names to list the roles (and a test)

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe Role do
+  it "has a non-empty list of role names" do
+    Role.all_role_names.length.should > 0
+  end
   pending "add some examples to (or delete) #{__FILE__}"
 end
