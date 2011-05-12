0261a13cfee8d78e880e7253d8197410d19283ea
David Grayson <davidegrayson@gmail.com>
2011-05-11 19:06:01 -0700

    Made the CSV file better: it uses commas and has fields in a better order.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Use commas in csv files
+ToCSV.csv_options[:col_sep] = ','
+
 # Initialize the rails application
 TrapQueue::Application.initialize!
