a4c58d286ab398ff336431a74cfdd76170189f8e
Russ Smith <russ@bashme.org>
2011-05-25 15:19:14 -0700

    Adding notes to jobs.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110524035107) do
+ActiveRecord::Schema.define(:version => 20110525221704) do
 
   create_table "bids", :force => true do |t|
     t.integer  "user_id"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(:version => 20110524035107) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "workflow_state",        :default => "open"
+    t.text     "notes"
   end
 
   create_table "locations", :force => true do |t|
